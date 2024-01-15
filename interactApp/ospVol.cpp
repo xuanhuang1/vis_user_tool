e9486e735505d2e61d8f1f502aa1e841a83afb2d
Xuan Huang <huangxuan2018@hotmail.com>
2024-01-14 20:26:29 -0700

    add glfw example

---
+++ interactApp/ospVol.cpp
@@ -0,0 +1,581 @@
+// Copyright 2009 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+/* This is a small example tutorial how to use OSPRay in an application.
+ *
+ * On Linux build it in the build_directory with
+ *   g++ ../apps/ospTutorial/ospTutorial.cpp -I ../ospray/include \
+ *       -I ../../rkcommon -L . -lospray -Wl,-rpath,. -o ospTutorial
+ * On Windows build it in the build_directory\$Configuration with
+ *   cl ..\..\apps\ospTutorial\ospTutorial.cpp /EHsc -I ..\..\ospray\include ^
+ *      -I ..\.. -I ..\..\..\rkcommon ospray.lib
+ * Above commands assume that rkcommon is present in a directory right "next
+ * to" the OSPRay directory. If this is not the case, then adjust the include
+ * path (alter "-I <path/to/rkcommon>" appropriately).
+ */
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../stb_image_write.h"
+
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <chrono>
+#ifdef _WIN32
+#define NOMINMAX
+#include <conio.h>
+#include <malloc.h>
+#include <windows.h>
+#else
+#include <alloca.h>
+#endif
+
+#include <vector>
+
+#include "ospray/ospray_cpp.h"
+#include "ospray/ospray_cpp/ext/rkcommon.h"
+#include "rkcommon/utility/SaveImage.h"
+
+#include "../loader.h"
+
+
+#define GLFW_INCLUDE_NONE
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+// imgui
+#include "./glfwImgui/imgui.h"
+#include "./glfwImgui/backends/imgui_impl_glfw.h"
+#include "./glfwImgui/backends/imgui_impl_opengl2.h"
+
+
+using namespace rkcommon::math;
+using json = nlohmann::json;
+using namespace visuser;
+
+
+// image size
+vec2i imgSize{800, 600};
+vec2i windowSize{800,600};
+unsigned int texture;
+unsigned int guiTextures[128];
+unsigned int guiTextureSize = 0;
+
+GLFWwindow *glfwWindow = nullptr;
+
+enum DATATYPE{TRI_MESH, VOL, TOTAL_DATA_TYPES};
+std::string dataTypeString[] = {"triangle_mesh", "volume"};
+
+
+class GLFWOSPWindow{
+public:
+  ospray::cpp::Camera camera{"perspective"};
+  ospray::cpp::Renderer renderer{"scivis"};
+  ospray::cpp::World world;
+  ospray::cpp::Instance instance;
+  
+  static GLFWOSPWindow *activeWindow;
+  ospray::cpp::FrameBuffer framebuffer;
+  //std::unique_ptr<ArcballCamera> arcballCamera;
+  
+  GLFWOSPWindow(){
+    activeWindow = this;
+    
+    /// prepare framebuffer
+    auto buffers = OSP_FB_COLOR | OSP_FB_DEPTH | OSP_FB_ACCUM | OSP_FB_ALBEDO
+      | OSP_FB_NORMAL;
+    framebuffer = ospray::cpp::FrameBuffer(imgSize.x, imgSize.y, OSP_FB_RGBA32F, buffers);
+    
+  }
+  
+  void display();
+  //void motion(double, double);
+  void mouse(int, int, int, int);
+  void reshape(int, int);
+    
+  void setFunc(){
+
+    //glfwSetCursorPosCallback(
+    //  glfwWindow, [](GLFWwindow *, double x, double y) {
+//		    activeWindow->motion(x, y);
+//		  });
+    glfwSetFramebufferSizeCallback(
+      glfwWindow, [](GLFWwindow *, int newWidth, int newHeight) {
+        activeWindow->reshape(newWidth, newHeight);
+      });
+
+    
+  }
+
+  void renderNewFrame(){
+    framebuffer.clear();
+    // render one frame
+    framebuffer.renderFrame(renderer, camera, world);
+  }
+
+  void buildUI();
+};
+
+GLFWOSPWindow *GLFWOSPWindow::activeWindow = nullptr;
+
+
+void GLFWOSPWindow::reshape(int w, int h)
+{
+  
+  windowSize.x = w;
+  windowSize.y = h;
+  
+  // create new frame buffer
+  auto buffers = OSP_FB_COLOR | OSP_FB_DEPTH | OSP_FB_ACCUM | OSP_FB_ALBEDO
+      | OSP_FB_NORMAL;
+  framebuffer =
+    ospray::cpp::FrameBuffer(imgSize.x, imgSize.y, OSP_FB_RGBA32F, buffers);
+  framebuffer.commit();
+  
+  glViewport(0, 0, windowSize.x, windowSize.y);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, windowSize.x, 0.0, windowSize.y, -1.0, 1.0);
+
+  // update camera
+  //arcballCamera->updateWindowSize(windowSize);
+
+  camera.setParam("aspect", windowSize.x / float(windowSize.y));
+  camera.commit();
+
+}
+
+void GLFWOSPWindow::display()
+{ 
+  
+   glBindTexture(GL_TEXTURE_2D, texture);
+   // render textured quad with OSPRay frame buffer contents
+   
+   renderNewFrame();
+   auto fb = framebuffer.map(OSP_FB_COLOR);
+
+   glTexImage2D(GL_TEXTURE_2D,
+		0,
+		GL_RGBA32F,
+		imgSize.x,
+		imgSize.y,
+		0,
+		GL_RGBA,
+		GL_FLOAT,
+		fb);
+   framebuffer.unmap(fb);
+   
+   
+   glBegin(GL_QUADS);
+
+   glTexCoord2f(0.f, 0.f);
+   glVertex2f(0.f, 0.f);
+
+   glTexCoord2f(0.f, 1.f);
+   glVertex2f(0.f, windowSize.y);
+
+   glTexCoord2f(1.f, 1.f);
+   glVertex2f(windowSize.x, windowSize.y);
+
+   glTexCoord2f(1.f, 0.f);
+   glVertex2f(windowSize.x, 0.f);
+
+   glEnd();
+}
+
+
+void GLFWOSPWindow::buildUI(){
+}
+
+ospray::cpp::TransferFunction makeTransferFunction(const vec2f &valueRange)
+{
+  ospray::cpp::TransferFunction transferFunction("piecewiseLinear");
+  std::string tfColorMap{"rgb"};
+  std::string tfOpacityMap{"linear"};
+  
+  std::vector<vec3f> colors;
+  std::vector<float> opacities;
+
+  if (tfColorMap == "jet") {
+    colors.emplace_back(0, 0, 0.562493);
+    colors.emplace_back(0, 0, 1);
+    colors.emplace_back(0, 1, 1);
+    colors.emplace_back(0.500008, 1, 0.500008);
+    colors.emplace_back(1, 1, 0);
+    colors.emplace_back(1, 0, 0);
+    colors.emplace_back(0.500008, 0, 0);
+  } else if (tfColorMap == "rgb") {
+    colors.emplace_back(0, 0, 1);
+    colors.emplace_back(0, 1, 0);
+    colors.emplace_back(1, 0, 0);
+  } else {
+    colors.emplace_back(0.f, 0.f, 0.f);
+    colors.emplace_back(1.f, 1.f, 1.f);
+  }
+
+  if (tfOpacityMap == "linear") {
+    opacities.emplace_back(0.f);
+    opacities.emplace_back(1.f);
+  } else if (tfOpacityMap == "linearInv") {
+    opacities.emplace_back(1.f);
+    opacities.emplace_back(0.f);
+  } else if (tfOpacityMap == "opaque") {
+    opacities.emplace_back(1.f);
+  }
+
+  transferFunction.setParam("color", ospray::cpp::CopiedData(colors));
+  transferFunction.setParam("opacity", ospray::cpp::CopiedData(opacities));
+  transferFunction.setParam("valueRange", valueRange);
+  transferFunction.commit();
+
+  return transferFunction;
+}
+
+void init (void* fb){
+    glEnable(GL_TEXTURE_2D);
+    glDisable(GL_LIGHTING);
+    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    //glEnable( GL_BLEND );
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+
+    glTexImage2D(GL_TEXTURE_2D,
+		 0,
+		 GL_RGBA32F,
+		 imgSize.x,
+		 imgSize.y,
+		 0,
+		 GL_RGBA,
+		 GL_FLOAT,
+		 fb);
+
+}
+
+int main(int argc, const char **argv)
+{
+
+
+#ifdef _WIN32
+    bool waitForKey = false;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+	// detect standalone console: cursor at (0,0)?
+	waitForKey = csbi.dwCursorPosition.X == 0 && csbi.dwCursorPosition.Y == 0;
+    }
+#endif
+
+    // initialize OSPRay; OSPRay parses (and removes) its commandline parameters,
+    // e.g. "--osp:debug"
+    OSPError init_error = ospInit(&argc, argv);
+    if (init_error != OSP_NO_ERROR)
+	return init_error;
+	
+    // use scoped lifetimes of wrappers to release everything before ospShutdown()
+    {
+    
+	// initialize GLFW
+	if (!glfwInit()) {
+	    throw std::runtime_error("Failed to initialize GLFW!");
+	}
+
+	glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
+	// create GLFW window
+	glfwWindow = glfwCreateWindow(windowSize.x, windowSize.y, "Viewer", nullptr, nullptr);
+
+	if (!glfwWindow) {
+	    glfwTerminate();
+	    throw std::runtime_error("Failed to create GLFW window!");
+	}
+	// make the window's context current
+	glfwMakeContextCurrent(glfwWindow);
+
+    
+	GLFWOSPWindow glfwOspWindow;
+
+
+	std::vector<std::string> args(argv, argv + argc);
+    
+	json config;
+	std::string prefix;
+	for (int i = 1; i < argc; ++i) {
+	    if (args[i] == "-h") {
+		std::cout << "./mini_vistool <config.json> [options]\n";
+		return 0;
+	    } else {
+		std::ifstream cfg_file(args[i].c_str());
+		if (!cfg_file) {
+		    std::cerr << "[error]: Failed to open config file " << args[i] << "\n";
+		    throw std::runtime_error("Failed to open input config file");
+		}
+		cfg_file >> config;
+	    }
+	}
+
+    
+	// load json
+	std::vector<Camera> cams = load_cameras(config["camera"].get<std::vector<json>>(), 10);
+	std::string dataType = config["data"]["type"];
+
+	// log out json info
+	{
+	    bool foundDataType = false;
+	    for( uint32_t i=0; i<TOTAL_DATA_TYPES; i++){
+		if (dataType == dataTypeString[i]){
+		    std::cout << "data type: " << dataType <<"\n";
+		    foundDataType = true;
+		}
+	    }
+	    if (!foundDataType) std::cerr << "Unsupported data type " << dataType <<"\n";
+      
+	    for(auto &c : cams)
+		c.print();
+	}
+
+
+	// construct ospray variables
+  
+	// image size
+	vec2i imgSize;
+	imgSize.x = 640; // width
+	imgSize.y = 480; // height
+
+	// camera
+	vec3f cam_pos{0.f, 0.f, 0.f};
+	vec3f cam_up{0.f, 1.f, 0.f};
+	vec3f cam_view{0.1f, 0.f, 1.f};
+
+	// triangle mesh data
+	std::vector<vec3f> vertex = {vec3f(-1.0f, -1.0f, 3.0f),
+	    vec3f(-1.0f, 1.0f, 3.0f),
+	    vec3f(1.0f, -1.0f, 3.0f),
+	    vec3f(1.0f, 1.0f, 3.0f)};
+
+	std::vector<vec4f> color = {vec4f(0.9f, 0.5f, 0.5f, 1.0f),
+	    vec4f(0.8f, 0.8f, 0.8f, 1.0f),
+	    vec4f(0.8f, 0.8f, 0.8f, 1.0f),
+	    vec4f(0.5f, 0.9f, 0.5f, 1.0f)};
+
+	std::vector<vec3ui> index = {vec3ui(0, 1, 2), vec3ui(1, 2, 3)};
+
+
+    
+	// create and setup camera
+	ospray::cpp::Camera camera("perspective");
+	camera.setParam("aspect", imgSize.x / (float)imgSize.y);
+	camera.setParam("position", cam_pos);
+	camera.setParam("direction", cam_view);
+	camera.setParam("up", cam_up);
+	camera.commit(); // commit each object to indicate modifications are done
+
+	ospray::cpp::Group group;
+	{
+	    vec3i volumeDimensions(100, 100, 100);
+	    std::vector<float> voxels(volumeDimensions.long_product());
+	    for (uint32_t i =0 ; i < voxels.size(); i++)
+		voxels[i] = float(i) / volumeDimensions.long_product();
+	    
+	    // volume
+	    ospray::cpp::Volume volume("structuredRegular");
+	    volume.setParam("gridOrigin", vec3f(-0.5f,-0.5f,1.5f));
+	    volume.setParam("gridSpacing", vec3f(1.f / reduce_max(volumeDimensions)));
+	    volume.setParam("data", ospray::cpp::CopiedData(voxels.data(), volumeDimensions));
+	    volume.setParam("dimensions", volumeDimensions);
+	    volume.commit();
+	    // put the mesh into a model
+	    ospray::cpp::VolumetricModel model(volume);
+	    model.setParam("transferFunction", makeTransferFunction(vec2f(0.f, 1.f)));
+	    model.commit();
+	    
+	    group.setParam("volume", ospray::cpp::CopiedData(model));
+	}
+	group.commit();
+
+	// put the group into an instance (give the group a world transform)
+	//ospray::cpp::Instance instance(group);
+	//instance.commit();
+    	glfwOspWindow.instance = ospray::cpp::Instance(group);
+    	glfwOspWindow.instance.commit();
+
+	// put the instance in the world
+	ospray::cpp::World world;
+	//world.setParam("instance", ospray::cpp::CopiedData(instance));
+	glfwOspWindow.world.setParam("instance", ospray::cpp::CopiedData(glfwOspWindow.instance));
+
+	// create and setup light for Ambient Occlusion
+	ospray::cpp::Light light("ambient");
+	light.commit();
+	
+	//world.setParam("light", ospray::cpp::CopiedData(light));
+	//world.commit();
+	glfwOspWindow.world.setParam("light", ospray::cpp::CopiedData(light));
+    	glfwOspWindow.world.commit();
+	
+	// create renderer, choose Scientific Visualization renderer
+	ospray::cpp::Renderer *renderer = &glfwOspWindow.renderer;;
+
+	// complete setup of renderer
+	renderer->setParam("aoSamples", 1);
+	renderer->setParam("backgroundColor", 1.0f); // white, transparent
+	renderer->commit();
+
+
+	if (1){
+	    ospray::cpp::Camera* camera = &glfwOspWindow.camera;
+	    for (int k=0; k<3; k++){
+			cam_pos[k] = cams[0].pos[k];
+			cam_up[k] = cams[0].up[k];
+			cam_view[k] = cams[0].dir[k];
+		}
+	    camera->setParam("position", cam_pos);
+	    camera->setParam("direction", cam_view);
+	    camera->setParam("up", cam_up);
+	    camera->commit();
+	    
+	    
+    glfwOspWindow.renderNewFrame();
+
+    
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(glfwWindow, true);
+    ImGui_ImplOpenGL2_Init();
+    
+    auto fb = glfwOspWindow.framebuffer.map(OSP_FB_COLOR);
+    init(fb);
+    glfwOspWindow.framebuffer.unmap(fb);
+    glfwOspWindow.setFunc();
+    glfwOspWindow.reshape(windowSize.x, windowSize.y);
+    glfwSetInputMode(glfwWindow, GLFW_STICKY_KEYS, GL_TRUE);
+    
+    auto t1 = std::chrono::high_resolution_clock::now();
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    do{
+      t1 = std::chrono::high_resolution_clock::now();
+      
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      glEnable(GL_TEXTURE_2D);
+      
+      
+      // Start the Dear ImGui frame
+      ImGui_ImplOpenGL2_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+      
+      
+      //glBindTexture(GL_TEXTURE_2D, h.texName);
+      glfwOspWindow.buildUI();
+      ImGui::Render();
+      
+      glEnable(GL_FRAMEBUFFER_SRGB); // Turn on sRGB conversion for OSPRay frame
+      
+      glfwOspWindow.display();
+      cam_pos[0] = (cam_pos[0] + 0.001);
+      camera->setParam("position", cam_pos);
+      camera->commit();
+      
+      glDisable(GL_FRAMEBUFFER_SRGB); // Disable SRGB conversion for UI
+      
+      glDisable(GL_TEXTURE_2D);
+      
+      ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+      
+      // Swap buffers
+      glfwSwapBuffers(glfwWindow);
+      
+      glfwPollEvents();
+
+      t2 = std::chrono::high_resolution_clock::now();
+      auto time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+      glfwSetWindowTitle(glfwWindow, (std::string("Render FPS:")+std::to_string(int(1.f / time_span.count()))).c_str());
+
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(glfwWindow, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+	   glfwWindowShouldClose(glfwWindow) == 0 );
+    
+   	ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+   	glfwTerminate();
+
+	}else{
+#if 0
+	    // create and setup framebuffer
+	    ospray::cpp::FrameBuffer framebuffer(
+						 imgSize.x, imgSize.y, OSP_FB_SRGBA, OSP_FB_COLOR | OSP_FB_ACCUM);
+	    framebuffer.clear();
+	    stbi_flip_vertically_on_write(1);
+
+	    // render frames
+	    for (size_t i=0; i<cams.size(); i++)
+		{
+		    for (int k=0; k<3; k++){
+			cam_pos[k] = cams[i].pos[k];
+			cam_up[k] = cams[i].up[k];
+			cam_view[k] = cams[i].dir[k];
+		    }
+		    camera.setParam("position", cam_pos);
+		    camera.setParam("direction", cam_view);
+		    camera.setParam("up", cam_up);
+		    camera.commit();
+	    
+		    framebuffer.renderFrame(renderer, camera, world);
+
+		    // access framebuffer and write its content as PPM file
+		    uint32_t *fb = (uint32_t *)framebuffer.map(OSP_FB_COLOR);
+		    std::string imgName = "outframe_osp_"+std::to_string(i)+".jpg";
+		    //rkcommon::utility::writePPM(imgName, imgSize.x, imgSize.y, fb);
+		    stbi_write_jpg(imgName.c_str(), imgSize.x, imgSize.y, 4, fb, 90);
+		    framebuffer.unmap(fb);
+		    framebuffer.clear();
+		    std::cout << "rendering frame to " << imgName << std::endl;
+
+		    // render 10 more frames, which are accumulated to result in a better
+		    // converged image
+		    //for (int frames = 0; frames < 10; frames++)
+		    //    framebuffer.renderFrame(renderer, camera, world);
+
+		    //fb = (uint32_t *)framebuffer.map(OSP_FB_COLOR);
+		    //rkcommon::utility::writePPM(
+		    //			    "accumulatedFrameCpp.ppm", imgSize.x, imgSize.y, fb);
+		    //framebuffer.unmap(fb);
+		    //std::cout << "rendering 10 accumulated frames to accumulatedFrameCpp.ppm"
+		    //	  << std::endl;
+
+		    ospray::cpp::PickResult res =
+			framebuffer.pick(renderer, camera, world, 0.5f, 0.5f);
+
+		    if (res.hasHit) {
+			std::cout << "picked geometry [instance: " << res.instance.handle()
+				  << ", model: " << res.model.handle()
+				  << ", primitive: " << res.primID << "]" << std::endl;
+		    }
+		    }
+		
+#endif
+		}
+    }
+    ospShutdown();
+
+#ifdef _WIN32
+    if (waitForKey) {
+	printf("\n\tpress any key to exit");
+	_getch();
+    }
+#endif
+
+    return 0;
+}
