f156e24dc46a92a272cdb032509f53c169fa78d8
Alper Şahıstan <alpersahistan@gmail.com>
2024-02-09 04:10:03 -0700

    Rough outline of a renderer

--- offlineApp/renderer.cpp
+++ offlineApp/renderer.cpp
@@ -35,7 +35,12 @@ ospray::cpp::TransferFunction makeTransferFunction(
 
     if (tfOpacityMap == "linear")
     {
+        opacities.emplace_back(1.f);
+        opacities.emplace_back(1.f);
         opacities.emplace_back(0.f);
+        opacities.emplace_back(0.f);
+        opacities.emplace_back(0.f);
+        opacities.emplace_back(1.f);
         opacities.emplace_back(1.f);
     }
     else if (tfOpacityMap == "linearInv")
@@ -58,7 +63,8 @@ ospray::cpp::TransferFunction makeTransferFunction(
 
 namespace visuser
 {
-
+    namespace rkm = rkcommon::math;
+    namespace ospray = ospray::cpp;
     Renderer::Renderer(const AniObjWidget &config)
     {
         // initialize OSPRay; OSPRay parses (and removes) its commandline parameters,
@@ -66,8 +72,10 @@ namespace visuser
         const char *argv[] = {"vistool_osp_offline"};
         InitializeOSPRay(1, argv);
 
-        ospray::cpp::Renderer renderer("scivis");
+        ospray::Renderer renderer("scivis");
         renderer.setParam("aoSamples", 0);
+        renderer.setParam("volumeSamplingRate", 0.008f);
+        renderer.setParam("backgroundColor", rkm::vec3f(0.3f, 0.3f, 0.3f));
         renderer.commit();
 
         // load umesh
@@ -86,34 +94,30 @@ namespace visuser
         // iterate over vertices and copy them into a vector
         for (auto &v : umeshHdlPtr->vertices)
         {
-            vertices.push_back(rkcommon::math::vec3f(v[0], v[1], v[2]));
+            vertices.push_back(rkcommon::math::vec3f(v.x, v.y, v.z));
         }
 
-        ospray::cpp::Volume volume("unstructured");
+        ospray::Volume volume("unstructured");
 
         // set data objects for volume object
-        volume.setParam("vertex.position", ospray::cpp::CopiedData(vertices));
-
-        volume.setParam("vertex.data", ospray::cpp::CopiedData(umeshHdlPtr->perVertex->values));
+        volume.setParam("vertex.position", ospray::CopiedData(vertices));
+        volume.setParam("vertex.data", ospray::CopiedData(umeshHdlPtr->perVertex->values));
+        volume.setParam("background", ospray::CopiedData(rkm::vec3f(1.0f, 0.0f, 1.0f)));
 
         // define cell offsets in indices array
-        std::vector<unsigned long> cells = {
-            0,                                                                                 // tets
-            umeshHdlPtr->tets.size(),                                                          // pyrs
-            umeshHdlPtr->tets.size() + umeshHdlPtr->pyrs.size(),                               // wedges
-            umeshHdlPtr->tets.size() + umeshHdlPtr->pyrs.size() + umeshHdlPtr->wedges.size()}; // hexes
+        std::vector<size_t> cells = {};
 
         // define cell types
         std::vector<uint8_t> cellTypes = {
-            OSP_TETRAHEDRON,
-            OSP_PYRAMID,
-            OSP_WEDGE,
-            OSP_HEXAHEDRON};
+            //OSP_TETRAHEDRON
+        };
 
-        std::vector<size_t> indices;
+        std::vector<uint64> indices;
         // loop over the elements and create a cummlative index list
         for (auto &t : umeshHdlPtr->tets)
         {
+            cellTypes.push_back(OSP_TETRAHEDRON);
+            cells.push_back(indices.size());
             indices.push_back(t[0]);
             indices.push_back(t[1]);
             indices.push_back(t[2]);
@@ -121,6 +125,8 @@ namespace visuser
         }
         for (auto &p : umeshHdlPtr->pyrs)
         {
+            cellTypes.push_back(OSP_PYRAMID);
+            cells.push_back(indices.size());
             indices.push_back(p[0]);
             indices.push_back(p[1]);
             indices.push_back(p[2]);
@@ -129,6 +135,8 @@ namespace visuser
         }
         for (auto &w : umeshHdlPtr->wedges)
         {
+            cellTypes.push_back(OSP_WEDGE);
+            cells.push_back(indices.size());
             indices.push_back(w[0]);
             indices.push_back(w[1]);
             indices.push_back(w[2]);
@@ -138,6 +146,8 @@ namespace visuser
         }
         for (auto &h : umeshHdlPtr->hexes)
         {
+            cellTypes.push_back(OSP_HEXAHEDRON);
+            cells.push_back(indices.size());
             indices.push_back(h[0]);
             indices.push_back(h[1]);
             indices.push_back(h[2]);
@@ -147,52 +157,89 @@ namespace visuser
             indices.push_back(h[6]);
             indices.push_back(h[7]);
         }
+        printf("#cells: %d #cell types %d\n", cells.size(), cellTypes.size());
 
-        volume.setParam("index", ospray::cpp::CopiedData(indices));
-        volume.setParam("cell.index", ospray::cpp::CopiedData(cells));
-        volume.setParam("cell.type", ospray::cpp::CopiedData(cellTypes));
+        volume.setParam("index", ospray::CopiedData(indices));
+        volume.setParam("cell.index", ospray::CopiedData(cells));
+        volume.setParam("cell.type", ospray::CopiedData(cellTypes));
 
         volume.commit();
 
-        ospray::cpp::VolumetricModel model(volume);
-        model.setParam("transferFunction", makeTransferFunction({0.f, 1.f}));
+        ospray::VolumetricModel model(volume);
+        model.setParam("transferFunction", makeTransferFunction({umeshHdlPtr->perVertex->valueRange.lower,umeshHdlPtr->perVertex->valueRange.upper }));
         model.commit();
 
-        ospray::cpp::Group group;
+        //get model bounds and print using ospGetBounds
 
-        group.setParam("volume", ospray::cpp::CopiedData(model));
+        ospray::Group group;
+
+        group.setParam("volume", ospray::CopiedData(model));
         group.commit();
-        ospray::cpp::Instance instance(group);
+        ospray::Instance instance(group);
         instance.commit();
 
-        // create world add group
-        ospray::cpp::World world;
-        world.setParam("instance", ospray::cpp::CopiedData(instance));
+        std::vector<ospray::Instance> inst;
+        inst.push_back(instance);
 
-        // calculate the bounding box of the umesh volume and set a camera to look at it
-        rkcommon::math::vec3f center = {umeshHdlPtr->bounds.center().x, umeshHdlPtr->bounds.center().y, umeshHdlPtr->bounds.center().z};
-        // get max extent of the bounding box
-        float maxExtent = std::max(umeshHdlPtr->bounds.size().x, std::max(umeshHdlPtr->bounds.size().y, umeshHdlPtr->bounds.size().z));
-        // set the camera position to the center of the bounding box and look at it
+        ospray::Light light("distant");
+        light.setParam("direction", rkm::vec3f(0.8f, -0.6f, 0.9f));
+        light.setParam("color", rkm::vec3f(0.78f, 0.551f, 0.483f));
+        light.setParam("intensity", 3.14f);
+        light.setParam("angularDiameter", 100.f);
+        light.commit();
+        ospray::Light ambient("ambient");
+        ambient.setParam("intensity", 0.35f);
+        ambient.setParam("visible", true);
+        ambient.commit();
+        std::vector<ospray::Light> lights{light, ambient};
 
-        arcballCamera.reset(
-            new ArcballCamera(world.getBounds<box3f>(), windowSize));
-        lastXfm = arcballCamera->transform();
+        ospray::World world;
+        world.setParam("instance", ospray::CopiedData(inst));
+        world.setParam("light", ospray::CopiedData(lights));
+        world.commit();
 
-        // init camera
-        camera.setParam("position", vec3f(0.0f, 0.0f, 1.0f));
-        updateCamera();
+        auto bounds = world.getBounds<rkm::box3f>();
+        std::cout << "bounds: " << bounds.lower.x << " " << bounds.lower.y << " " << bounds.lower.z << ", " << bounds.upper.x << " " << bounds.upper.y << " " << bounds.upper.z << std::endl;
+        //print the umesh bounds
+        std::cout << "umesh bounds: " << umeshHdlPtr->bounds.lower << " " << umeshHdlPtr->bounds.upper << std::endl;
+
+        //calculate center of the bounds and set the camera diretion to the center and position to the center - 5
+        rkm::vec3f center = (bounds.lower + bounds.upper) / 2.f;
+        rkm::vec3f cameraPos = center - rkm::vec3f(1.001f, 1.3f, 0.f) * 90.f;
+        rkm::vec3f cameraDir = center - cameraPos;
+
+        // create and setup camera
+        ospray::Camera camera("perspective");
+        camera.setParam("aspect", 1.0f);
+        camera.setParam("position", cameraPos);
+        camera.setParam("direction", cameraDir);
+        camera.setParam("up", rkm::vec3f({0.f, -1.f, 0.f}));
         camera.commit();
 
-        ospray::cpp::FrameBuffer framebuffer(1024, 1024, OSP_FB_SRGBA, OSP_FB_COLOR | OSP_FB_ACCUM);
-        framebuffer.clear();
-
-        // Render
-        framebuffer.renderFrame(renderer, camera, world);
-
-        // Access rendered image
-        const uint32_t *fb = (uint32_t *)framebuffer.map(OSP_FB_COLOR);
-        rkcommon::utility::writePPM("output.ppm", 1024, 1024, fb);
+         for (int i = 0; i < 10; ++i) {
+            // Calculate new camera position for rotation
+            float angle = i * (2.0f * M_PI / 10.0f); // 10 frames for a full circle
+            rkm::vec3f cameraPos = center + rkm::vec3f(std::cos(angle) * 90.f, std::sin(angle) * 90.f, 0.f);
+            rkm::vec3f cameraDir = center - cameraPos;
+            
+            // Setup camera
+            ospray::Camera camera("perspective");
+            camera.setParam("aspect", 1.0f);
+            camera.setParam("position", cameraPos);
+            camera.setParam("direction", cameraDir);
+            camera.setParam("up", rkm::vec3f({0.001f, -1.f, -0.01f}));
+            camera.commit();
+            
+            // Render frame
+            ospray::FrameBuffer framebuffer(1024, 1024, OSP_FB_SRGBA, OSP_FB_COLOR | OSP_FB_ACCUM);
+            framebuffer.clear();
+            framebuffer.renderFrame(renderer, camera, world);
+            
+            // Access and save rendered image
+            const uint32_t *fb = (uint32_t *)framebuffer.map(OSP_FB_COLOR);
+            rkcommon::utility::writePPM("output_frame_" + std::to_string(i) + ".ppm", 1024, 1024, fb);
+        }
+        //update the camera position to move up
     }
 
     void Renderer::Render()
