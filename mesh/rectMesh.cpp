82b10236ec19ef61fd5e05658ddfb71842f4151c
Xuan Huang <huangxuan2018@hotmail.com>
2024-02-23 15:38:25 -0700

    minor refactor with rectMesh

--- mesh/rectMesh.cpp
+++ mesh/rectMesh.cpp
@@ -7,39 +7,9 @@ namespace visuser
         std::vector<float> z_mapping, std::string rawFilePath,
         rkm::vec2f xy_scaling)
     {
-        // create vertices
-        for (int k = 0; k < dimensions.z; k++)
-        {
-            for (int j = 0; j < dimensions.y; j++)
-            {
-                for (int i = 0; i < dimensions.x; i++)
-                {
-                    vertices.push_back(rkm::vec3f(i * xy_scaling.x, j * xy_scaling.y, z_mapping[k]));
-                }
-            }
-        }
+	initMesh(dimensions, z_mapping, xy_scaling);
 
-        // create indices in vtk hexahedron format
-        for (int k = 0; k < dimensions.z - 1; k++)
-        {
-            for (int j = 0; j < dimensions.y -1; j++)
-            {
-                for (int i = 0; i < dimensions.x - 1; i++)
-                {
-                    //bottom half I, II, III, IV
-                    indices.push_back(i + j * dimensions.x + k * dimensions.x * dimensions.y);
-                    indices.push_back(i + 1 + j * dimensions.x + k * dimensions.x * dimensions.y);
-                    indices.push_back(i + 1 + (j + 1) * dimensions.x + k * dimensions.x * dimensions.y); 
-                    indices.push_back(i + (j + 1) * dimensions.x + k * dimensions.x * dimensions.y);
-                    //top half V, VI, VII, VIII
-                    indices.push_back(i + j * dimensions.x + (k + 1) * dimensions.x * dimensions.y);
-                    indices.push_back(i + 1 + j * dimensions.x + (k + 1) * dimensions.x * dimensions.y);
-                    indices.push_back(i + 1 + (j + 1) * dimensions.x + (k + 1) * dimensions.x * dimensions.y);
-                    indices.push_back(i + (j + 1) * dimensions.x + (k + 1) * dimensions.x * dimensions.y);
-                }
-            }
-        }
-
+	// Read from file
         std::ifstream inputFile(rawFilePath, std::ios::binary);
         if (!inputFile.is_open()) {
             std::cerr << "Error: Failed to open file " << rawFilePath << std::endl;
@@ -53,5 +23,61 @@ namespace visuser
         // Close the file
         inputFile.close();
     }
+
+    
+    RectMesh::RectMesh(rkm::vec3ui dimensions, 
+		       std::vector<float> z_mapping,
+		       std::vector<float> &v,
+		       rkm::vec2f xy_scaling)
+    {
+	initMesh(dimensions, z_mapping, xy_scaling);
+
+	// Read from array
+	setValue(v);
+    }
+
+    void RectMesh::initMesh(rkm::vec3ui dimensions, 
+			    std::vector<float> &z_mapping, 
+			    rkm::vec2f xy_scaling)
+    {
+	// create vertices
+        for (int k = 0; k < dimensions.z; k++)
+	    {
+		for (int j = 0; j < dimensions.y; j++)
+		    {
+			for (int i = 0; i < dimensions.x; i++)
+			    {
+				vertices.push_back(rkm::vec3f(i * xy_scaling.x, j * xy_scaling.y, z_mapping[k]));
+			    }
+		    }
+	    }
+
+        // create indices in vtk hexahedron format
+        for (int k = 0; k < dimensions.z - 1; k++)
+	    {
+		for (int j = 0; j < dimensions.y -1; j++)
+		    {
+			for (int i = 0; i < dimensions.x - 1; i++)
+			    {
+				//bottom half I, II, III, IV
+				indices.push_back(i + j * dimensions.x + k * dimensions.x * dimensions.y);
+				indices.push_back(i + 1 + j * dimensions.x + k * dimensions.x * dimensions.y);
+				indices.push_back(i + 1 + (j + 1) * dimensions.x + k * dimensions.x * dimensions.y); 
+				indices.push_back(i + (j + 1) * dimensions.x + k * dimensions.x * dimensions.y);
+				//top half V, VI, VII, VIII
+				indices.push_back(i + j * dimensions.x + (k + 1) * dimensions.x * dimensions.y);
+				indices.push_back(i + 1 + j * dimensions.x + (k + 1) * dimensions.x * dimensions.y);
+				indices.push_back(i + 1 + (j + 1) * dimensions.x + (k + 1) * dimensions.x * dimensions.y);
+				indices.push_back(i + (j + 1) * dimensions.x + (k + 1) * dimensions.x * dimensions.y);
+			    }
+		    }
+	    }
+	
+    }
+
+    void RectMesh::setValue(std::vector<float> &v)
+    {
+	for (size_t i=0; i<v.size(); i++) scalars.push_back(v[i]);
+    }    
 
 }
--- mesh/rectMesh.h
+++ mesh/rectMesh.h
@@ -6,10 +6,21 @@ namespace visuser
     class RectMesh
     {
     public:
+	RectMesh(){};
+    	RectMesh(rkm::vec3ui dimensions, 
+		 std::vector<float> z_mapping,
+		 std::vector<float> &v,
+		 rkm::vec2f xy_scaling = {1.0f, 1.0f});
         RectMesh(rkm::vec3ui dimensions, 
-            std::vector<float> z_mapping, std::string rawFilePath,
-            rkm::vec2f xy_scaling = {1.0f, 1.0f});
+		 std::vector<float> z_mapping,
+		 std::string rawFilePath,
+		 rkm::vec2f xy_scaling = {1.0f, 1.0f});
 
+	void initMesh(rkm::vec3ui dimensions, 
+		      std::vector<float> &z_mapping,
+		      rkm::vec2f xy_scaling);
+	void setValue(std::vector<float> &v);
+	
         std::vector<float> scalars;
         std::vector<rkm::vec3f> vertices;
         std::vector<size_t> indices;
