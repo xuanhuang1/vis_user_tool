7930f42035ab493da284f539212248f497b47dbd
Xuan Huang <huangxuan2018@hotmail.com>
2024-07-21 18:44:10 -0600

    example script for frog

--- AIExample/py/scripts/test.py
+++ AIExample/py/scripts/test.py
@@ -4,6 +4,14 @@ scriptdir = os.path.split(__file__)[0]
 sys.path.insert(0, os.path.join(scriptdir, '..'))
 
 import numpy
-import vistool_gl_py
+import vistool_ai_py
 
-vistool_gl_py.echo(10)
+vistool_ai_py.frog_helper();
+
+rndr = vistool_ai_py.RendererHandler()
+rndr.rotateUp(0.5)
+rndr.setIsoValueVolumeRange(200)
+rndr.setRenderMode("volume")
+
+frog_renderer_dir = "/home/xuanhuang/projects/vis_interface/vis_user_tool/AIExample" 
+vistool_ai_py.run_frog(frog_renderer_dir, rndr)
