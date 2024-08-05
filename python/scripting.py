eda41dddec0911d0fa9afa0af62cd792e12b2570
Xuan Huang <huangxuan2018@hotmail.com>
2024-08-05 14:21:54 -0600

    add relative path in python

--- python/scripting.py
+++ python/scripting.py
@@ -3,7 +3,8 @@ import numpy as np
 from threading import Thread
 
 # path to the rendering app libs
-sys.path.append("/home/xuanhuang/projects/vis_interface/vis_user_tool/build/renderingApps/py/")
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'build', 'renderingApps', 'py'))
+#sys.path.append("/home/xuanhuang/projects/vis_interface/vis_user_tool/build/renderingApps/py/")
 import renderInterface
 
 #
